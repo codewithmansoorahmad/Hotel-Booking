abac5e2eb752dfaa3c9bbf72ec30fdd487635dcf
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-21 21:58:34 +0500

    Arrow right icons was added with css

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,10 +1,12 @@
+import { ArrowRight } from "lucide-react";
+
 export default function HomeHero(){
     return <div className="home-hero">
         <div className="hero-info">
             <h1>Welcome to StayFInder</h1>
             <h2>Find Your Perfect Stay</h2>
             <p>Discover comofortable hotes and distinations for your next journey</p>
-            <button>Explore Hotels</button>
+            <button>Explore Hotels<ArrowRight size={15}/></button>
         </div>
 
     </div>
