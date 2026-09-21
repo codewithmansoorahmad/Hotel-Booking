4759e73d4825bb5529ed2b56f92a72960256ac35
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-21 21:54:28 +0500

    add hero-info

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,5 +1,11 @@
 export default function HomeHero(){
     return <div className="home-hero">
-        
+        <div className="hero-info">
+            <h1>Welcome to StayFInder</h1>
+            <h2>Find Your Perfect Stay</h2>
+            <p>Discover comofortable hotes and distinations for your next journey</p>
+            <button>Explore Hotels</button>
+        </div>
+
     </div>
 }
