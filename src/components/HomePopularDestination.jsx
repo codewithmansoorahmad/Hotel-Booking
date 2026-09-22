ccc7c1094b801e36f8b91fe586ba0923c11851f7
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-22 21:32:15 +0500

    Add css to homepopular recipes

--- src/components/HomePopularDestination.jsx
+++ src/components/HomePopularDestination.jsx
@@ -1,22 +1,27 @@
+import { ArrowRight, MapPin } from "lucide-react"
 import { popularDestinations } from "../JS service/DestinationsCode"
 
 export default function HomePopularDestinations(){
     let destiations=popularDestinations
     return <div className="home-destinations">
         <div className="popular-info">
+            <div>
             <h1>Explore</h1>
             <h2>Popular Destinations</h2>
             <p>Discover amazing places and find perfect stay.</p>
-
+</div>
+<div>
+<button>View All destinations <ArrowRight/></button>
+</div>
         </div>
-        <div className="destinations-grid">
 
+        <div className="destinations-grid">
         {
-            destiations.map((item)=>{
-return <div className="destination">
+            destiations.map((item,index)=>{
+return <div className="destination" key={index}>
     <img src={item.image} alt="" />
     <p className="name">{item.name}</p>
-    <p>{item.province}</p>
+    <p><MapPin size={14}/>{item.province}</p>
 </div>
             })
         }
