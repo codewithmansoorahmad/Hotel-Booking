afbb7e6f8334e6bca2f1504609f1ae066d594b1d
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-22 19:39:08 +0500

    add setisicons to all navlinks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import { Heart } from "lucide-react"
 export default function NavBar({isIcon,setIsIcon}){
     return <nav className="nav-bar">
         <ul className={isIcon?"ul-list":"ul-open-list"}>
-<li className="li"><NavLink className="link">Home</NavLink></li>
-<li className="li"><NavLink className="link">About</NavLink></li>
-<li className="li"><NavLink className="link">Hotels</NavLink></li>
-<li className="li"><NavLink className="link">Destinations</NavLink></li>
-<li className="li"><NavLink className="link-favorite"><Heart size={20}/> <span>favorites</span></NavLink></li>
+<li className="li"><NavLink onClick={()=>setIsIcon(true)}  to="/"  className="link">Home</NavLink></li>
+<li className="li"><NavLink onClick={()=>setIsIcon(true)} to="/about" className="link">About</NavLink></li>
+<li className="li"><NavLink onClick={()=>setIsIcon(true)} to="/hotels" className="link">Hotels</NavLink></li>
+<li className="li"><NavLink onClick={()=>setIsIcon(true)} to="/destinations" className="link">Destinations</NavLink></li>
+<li className="li"><NavLink onClick={()=>setIsIcon(true)} to="/favorites" className="link-favorite"><Heart size={20}/> <span>favorites</span></NavLink></li>
 
 
         </ul>
