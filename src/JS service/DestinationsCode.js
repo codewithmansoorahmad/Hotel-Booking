c7c1303439a6d2f8edb4a3e43f4c47041ddecdab
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-22 21:07:36 +0500

    create popular destinations array

---
+++ src/JS service/DestinationsCode.js
@@ -0,0 +1,38 @@
+import swatValley from "../assets/swatValley.avif"
+import peshawar from "../assets/Peshawar.avif"
+import lahore from "../assets/lahore.avif"
+import islamabad from "../assets/islamabad.avif"
+import karachi from "../assets/karachi.avif"
+import hunza from "../assets/hunza.avif"
+export const popularDestinations=[
+    {
+        name:"Swat",
+        province:"khyberPakhtunkwa",
+        imageg:swatValley,
+    },
+    {
+        name:"Peshawar",
+        province:"khyberPakhtunkwa",
+        imageg:peshawar,
+    },
+    {
+        name:"Islamabad",
+        province:"Islamabad",
+        imageg:islamabad,
+    },
+    {
+        name:"Lahore",
+        province:"Punjab",
+        imageg:lahore,
+    },
+    {
+        name:"karachi",
+        province:"Sindh",
+        imageg:karachi,
+    },
+    {
+        name:"Hunza",
+        province:"Gilgit",
+        imageg:hunza,
+    },
+]
