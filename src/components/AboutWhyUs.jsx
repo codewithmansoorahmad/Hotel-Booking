4e1775496de979af7f0e8a10c1c2706eb4e6ad1d
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-23 21:19:10 +0500

    Add why us css

--- src/components/AboutWhyUs.jsx
+++ src/components/AboutWhyUs.jsx
@@ -3,11 +3,11 @@ import { Check } from "lucide-react";
 export default function AboutWhyUs(){
     return <div className="why-us">
         <h1>Why StayFInder?</h1>
-        <p><Check/>Popular Destinations</p>
-        <p><Check/>Easy Hotels Discover</p>
-        <p><Check/>Simple Booking Experience</p>
-        <p><Check/>Save Your Favorite Hotels</p>
-        <p><Check/>Getting Fast Response</p>
-        <p><Check/>Getting Discount through Us</p>
+        <p><Check className="check-icon"/>Popular Destinations</p>
+        <p><Check className="check-icon"/>Easy Hotels Discover</p>
+        <p><Check className="check-icon"/>Simple Booking Experience</p>
+        <p><Check className="check-icon"/>Save Your Favorite Hotels</p>
+        <p><Check className="check-icon"/>Getting Fast Response</p>
+        <p><Check className="check-icon"/>Getting Discount through Us</p>
     </div>
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import AboutHero from "../components/AboutHero"
 import AboutPakistan from "../components/AboutPakistan"
 import AboutStay from "../components/AboutStay"
+import AboutWhyUs from "../components/AboutWhyUs"
 import '../css/About.css'
 export default function About(){
    return <div className="about">
        <AboutHero/>
        <AboutStay/>
        <AboutPakistan/>
+       <AboutWhyUs/>
     </div>
 }
