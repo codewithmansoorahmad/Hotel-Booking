dcd5accf79427d06a28c527bc5b98308f254c281
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-23 16:44:13 +0500

    add alldestinations code in array

--- src/JS service/DestinationsCode.js
+++ src/JS service/DestinationsCode.js
@@ -4,35 +4,165 @@ import lahore from "../assets/lahore.avif"
 import islamabad from "../assets/islamabad.avif"
 import karachi from "../assets/karachi.avif"
 import hunza from "../assets/hunza.avif"
+import abbotatbad from "../assets/abbotatbad.avif"
+import chitral from "../assets/chitral.avif"
+import dir from "../assets/dir.avif"
+import faisalabad from "../assets/faisalabad.avif"
+import multan from "../assets/Multan.avif"
+import naran from "../assets/naran.avif"
+import nathiaGali from "../assets/nathiaGali.avif"
+import quetta from "../assets/Quetta.avif"
+import rawalpindi from "../assets/Rawalpindi.avif"
+import skardu from "../assets/Skardu.avif"
 export const popularDestinations=[
     {
         name:"Swat",
         province:"khyberPakhtunkwa",
         image:swatValley,
+        id:1
     },
     {
         name:"Peshawar",
         province:"khyberPakhtunkwa",
         image:peshawar,
+        id:2
     },
     {
         name:"Islamabad",
         province:"Islamabad",
         image:islamabad,
+        id:3
     },
     {
         name:"Lahore",
         province:"Punjab",
         image:lahore,
+        id:4
+
     },
     {
         name:"karachi",
         province:"Sindh",
         image:karachi,
+        id:5
+
     },
     {
         name:"Hunza",
         province:"Gilgit",
         image:hunza,
+        id:6
+
     },
+]
+
+
+
+export const allDestinations=[
+       {
+        name:"Swat",
+        province:"khyberPakhtunkwa",
+        image:swatValley,
+        id:1
+    },
+    {
+        name:"Peshawar",
+        province:"khyberPakhtunkwa",
+        image:peshawar,
+        id:2
+    },
+    {
+        name:"Islamabad",
+        province:"Islamabad",
+        image:islamabad,
+        id:3
+    },
+    {
+        name:"Lahore",
+        province:"Punjab",
+        image:lahore,
+        id:4
+
+    },
+    {
+        name:"karachi",
+        province:"Sindh",
+        image:karachi,
+        id:5
+
+    },
+    {
+        name:"Hunza",
+        province:"Gilgit",
+        image:hunza,
+        id:5
+
+    },
+    {
+        name:"Hunza",
+        province:"Gilgit",
+        image:hunza,
+        id:5
+
+    },
+    {
+    name: "Abbottabad",
+    province: "khyberPakhtunkhwa",
+    image: abbotatbad,
+    id: 7
+},
+{
+    name: "Chitral",
+    province: "khyberPakhtunkhwa",
+    image: chitral,
+    id: 8
+},
+{
+    name: "Dir",
+    province: "khyberPakhtunkhwa",
+    image: dir,
+    id: 9
+},
+{
+    name: "Faisalabad",
+    province: "punjab",
+    image: faisalabad,
+    id: 10
+},
+{
+    name: "Multan",
+    province: "punjab",
+    image: multan,
+    id: 11
+},
+{
+    name: "Naran",
+    province: "khyberPakhtunkhwa",
+    image: naran,
+    id: 12
+},
+{
+    name: "Nathia Gali",
+    province: "khyberPakhtunkhwa",
+    image: nathiaGali,
+    id: 13
+},
+{
+    name: "Quetta",
+    province: "balochistan",
+    image: quetta,
+    id: 14
+},
+{
+    name: "Rawalpindi",
+    province: "punjab",
+    image: rawalpindi,
+    id: 15
+},
+{
+    name: "Skardu",
+    province: "gilgitBaltistan",
+    image: skardu,
+    id: 16
+}
 ]
