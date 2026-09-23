dbb6999b8a1a0f2ee4a1cba27760f12c05219635
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-23 21:14:37 +0500

    Add WHyUs component

---
+++ src/components/AboutWhyUs.jsx
@@ -0,0 +1,13 @@
+import { Check } from "lucide-react";
+
+export default function AboutWhyUs(){
+    return <div className="why-us">
+        <h1>Why StayFInder?</h1>
+        <p><Check/>Popular Destinations</p>
+        <p><Check/>Easy Hotels Discover</p>
+        <p><Check/>Simple Booking Experience</p>
+        <p><Check/>Save Your Favorite Hotels</p>
+        <p><Check/>Getting Fast Response</p>
+        <p><Check/>Getting Discount through Us</p>
+    </div>
+}
