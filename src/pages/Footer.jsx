357f9b0e029ed74e958e0de9f2354cde739bc8a9
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-23 22:09:43 +0500

    footer Link CHange TO NavLInk

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,21 +1,21 @@
-import { Home } from "lucide-react";
-import { Link } from "react-router-dom";
+import { House } from "lucide-react";
+import { NavLink } from "react-router-dom";
 import "../css/Footer.css"
 
 export default function Footer(){
    return <div className="footer">
         <div className="stay-finder">
           
-            <h1><Home className="home-icon"/>StayFinder</h1>
+            <h1><House className="home-icon" size={30} strokeWidth={1}/>StayFinder</h1>
             <p>Find comfortable stays and explore destinations across pakistan.</p>
         </div>
-        <div className="quickLinks">
-            <ul className="links-ul">
-                <Link className="quick-link">Home</Link>
-                <Link className="quick-link">About</Link>
-                <Link className="quick-link">Hotels</Link>
-                <Link className="quick-link">Destinations</Link>
-                <Link className="quick-link">Favorites</Link>
+        <div className="quickNavLinks">
+            <ul className="NavLinks-ul">
+                <NavLink to="/" className="quick-NavLink">Home</NavLink>
+                <NavLink to="/About" className="quick-NavLink">About</NavLink>
+                <NavLink to="/Hotels" className="quick-NavLink">Hotels</NavLink>
+                <NavLink to="/Destinations" className="quick-NavLink">Destinations</NavLink>
+                <NavLink to="/Favorites" className="quick-NavLink">Favorites</NavLink>
             </ul>
         </div>
         <div className="footer-footer">
