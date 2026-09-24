c8ee0866583e4c5e9c041a248ef828034e6d0b42
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-24 16:34:40 +0500

    Add css to destinationmain

--- src/components/DestinationsMain.jsx
+++ src/components/DestinationsMain.jsx
@@ -2,8 +2,15 @@ import DestinationsAll from "./DestinationsAll";
 
 export default function DestinationsMain({selectVal,setSelectVal,destinations,setDestinations}){
     return <div className="main-destinations">
-        <h1>-----------Explore Destinations------------</h1>
-        <p>Select Destinations and get hotels</p>
+        <h1>---Explore Destinations---</h1>
+       
+<p>Select a destination and find hotels for your stay.</p>
+
+<h2>Explore Hotels by Destination</h2>
+
+<p>Choose a destination to discover hotels for your stay.</p>
+
+<p>Available Destinations with StayFinder</p>
         <label htmlFor="select">Choose Province</label>
         <select  id="select" value={selectVal} onChange={(e)=>setSelectVal(e.target.value)} >
             <option value="All">All</option>
@@ -16,6 +23,10 @@ export default function DestinationsMain({selectVal,setSelectVal,destinations,se
 
 
         <DestinationsAll selectVal={selectVal} setDestinations={setDestinations} destinations={destinations}/>
+
+<p>
+Find the perfect place for your next journey and explore comfortable stays with StayFinder.</p>
+
 
     </div>
 }
